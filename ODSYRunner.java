57dedcb1bf98fb385bcc01d5aa8d4fef528090e4
PatHealy <pwh5@pitt.edu>
2017-03-19 06:09:58 -0400

    Initialized runner class

---
+++ ODSYRunner.java
@@ -0,0 +1,17 @@
+public class ODSYRunner(){
+
+    int gameChoice = 0;
+    
+    private static void decrementGame(){
+        
+    }
+    
+    private static void incrementGame(){
+        
+    }
+    
+    public static void main(String[] args){
+        //there is a lot of work to be done
+    }
+
+}
