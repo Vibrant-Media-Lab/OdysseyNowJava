81d07092ce2b80d76d8dea70c3eb2cac7dbf69e0
PatHealy <pwh5@pitt.edu>
2017-03-21 02:00:44 -0400

    Added player box object. Game is now a simple moving box using keyboard arrows

--- ODSYRunner.java
+++ ODSYRunner.java
@@ -1,49 +1,64 @@
-package ODSYredux;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-import java.applet.Applet;
-
-public class ODSYRunner extends Applet{
-
-    int gameChoice = 0;
+@SuppressWarnings("serial")
+public class ODSYRunner extends JPanel {
     
-    public void init(){
-        
+    int leftX = 0;
+    int leftY = 0;
+    int rightX = 0;
+    int rightY = 0;
+    
+    playerBox box1 = new playerBox(this);
+    
+    public ODSYRunner(){
+        addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				box1.keyReleased(e);
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				box1.keyPressed(e);
+			}
+		});
+		setFocusable(true);
     }
     
-    public void paint(Graphics g){
+
+	private void moveBox() {
+		box1.move();
+	}
+    
+    
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+            RenderingHints.VALUE_ANTIALIAS_ON);
+        box1.paint(g2d);
         
     }
-    
-    public void update(Graphics g){ //main system tick function
-        boolean exitTime = false;
-        while(true){
-            
-            if(exitTime){
-                exit();
-            }
-            
-            try{
-                Thread.sleep(10);
-            } catch(InterruptedException e){
-                e.printStackTrace();
-            }
-            
+
+    public static void main(String[] args) throws InterruptedException {
+        JFrame frame = new JFrame("Sample Frame");
+        ODSYRunner game = new ODSYRunner();
+        frame.add(game);
+        frame.setSize(300, 400);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        while (true) {
+            game.moveBox();
+            game.repaint();
+            Thread.sleep(10);
         }
     }
-    
-    private void exit(){
-        System.exit(0);
-    }
-    
-    
-    private void decrementGame(){
-        
-    }
-    
-    private void incrementGame(){
-        
-    }
-    
-    
-
 }
---
+++ playerBox.java
@@ -0,0 +1,49 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class playerBox{
+	int x = 0;
+	int y = 0;
+	int xa = 1;
+	int ya = 0;
+	private ODSYRunner game;
+
+	public playerBox(ODSYRunner game) {
+		this.game = game;
+	}
+
+	void move() {
+		if (x + xa < 0)
+			xa = 1;
+		if (x + xa > game.getWidth() - 30)
+			xa = -1;
+		if (y + ya < 0)
+			ya = 1;
+		if (y + ya > game.getHeight() - 30)
+			ya = -1;
+
+		x = x + xa;
+		y = y + ya;
+	}
+
+	public void paint(Graphics2D g) {
+		g.fillRect(x, y, 30, 30);
+	}
+    
+    public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+			xa = -1;
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			xa = 1;
+        
+        if (e.getKeyCode() == KeyEvent.VK_UP)
+			ya = -1;
+		if (e.getKeyCode() == KeyEvent.VK_DOWN)
+			ya = 1;
+	}
+    
+    public void keyReleased(KeyEvent e) {
+		xa = 0;
+        ya = 0;
+	}
+}
